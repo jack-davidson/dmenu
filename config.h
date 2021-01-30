445a33f78789a5f629feff3100c370815012db37
jack-davidson <jackmaverickdavidson@gmail.com>
2021-01-30 13:40:13 -0600

    added patches

--- config.h
+++ config.h
@@ -2,16 +2,26 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int fuzzy = 1;
+static const char *prompt = NULL;      /* -p  option; prompt to the left of input field */
+
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[] = {
-	"monospace:size=10"
-};
-static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *fonts[] = {"monospace:size=10"};
+
+
+
+static const char bg0[]     = "#282828";
+static const char bg1[]    = "#3c3836";
+
+static const char fg0[]     = "#ebdbb2";
+static const char fg1[]     = "#d79921";
+
 static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
+	/*                        fg   bg  */
+	[SchemeNorm]          = { fg0, bg0 },
+	[SchemeSel]           = { fg0, bg1 },
+	[SchemeNormHighlight] = { fg1, bg0 },
+	[SchemeSelHighlight]  = { fg1, bg1 },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
